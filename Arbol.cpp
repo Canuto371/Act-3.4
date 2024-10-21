fee8d875ebb586d68c6d7131960840a5ded7891f
eli 3 <157757893+Canuto371@users.noreply.github.com>
2024-10-21 06:41:50 +0000

    lol2

--- Arbol.cpp
+++ Arbol.cpp
@@ -51,6 +51,8 @@ Arbol::Arbol(){
 }
 
 // La complejidad en el peor caso es O(n), donde n es la altura del árbol.
+// El método realiza una comparación en cada nivel del árbol para decidir si inserta en el subárbol izquierdo o derecho.
+// En el peor de los casos, la inserción puede requerir recorrer la altura completa del árbol.
 Nodo* Arbol::insertarNodo(Nodo *r, int d) {
     if (r == nullptr) {
         return new Nodo(d);
@@ -68,11 +70,12 @@ Nodo* Arbol::insertarNodo(Nodo *r, int d) {
     return r;
 }
 
-// Complejidad en caso promedio de O(n log n)
+// Complejidad en el peor caso es de O(n^2), donde insertar un nodo tiene una complejidad O(n), y el bucle se itera n veces.
 void Arbol::leerNodosDesdeArchivo(const string& nombreArchivo) {
     ifstream archivo(nombreArchivo);
 
     // Complejidad O(n), donde n es el número de elementos en el archivo.
+    // Se lee un número entero desde el archivo, que tiene una complejidad O(1).
     int numeroDeElementos;
     archivo >> numeroDeElementos;
 
@@ -81,8 +84,9 @@ void Arbol::leerNodosDesdeArchivo(const string& nombreArchivo) {
         return;
     }
 
-    // Complejidad temporal de O(n), donde n es la altura del árbol.
+    // Complejidad temporal de O(n), donde n son el número de nodos.
     // Se insertan los elementos del .txt en nodos para formar el árbol
+    // Si hay n elementos en el archivo, el bucle se ejecuta n veces.
     for (int i = 0; i < numeroDeElementos; i++) {
         int valor;
         if (archivo >> valor) {
@@ -98,6 +102,8 @@ void Arbol::leerNodosDesdeArchivo(const string& nombreArchivo) {
 
 // La complejidad en el peor caso es O(n), donde n es el número de nodos en el árbol.
 // Método para realizar el recorrido en inOrden	
+// Cada nodo se visita una vez y se realiza una operación constante para cada nodo. 
+// Por lo tanto, el número total de llamadas recursivas es proporcional a la cantidad de nodos en el árbol.
 void Arbol::inOrden(Nodo* r, vector<int>& datos) {
     if (r != nullptr) {
         inOrden(r->getLeft(), datos);
@@ -118,8 +124,10 @@ void Arbol::imprimirVector(const vector<int>& datos) {
 
 // Complejidad temporal de O(n^2), donde n es el número inicial de elementos en el vector.
 // Las operaciones de eliminación e inserción tienen relación directa con el tamaño del vector, que disminuye en cada paso.
+// O(n) para cada iteración, y como hay n−1 iteraciones, la complejidad total es O(n^2).
 void Arbol::minComparaciones(vector<int>& datos) {
     int comparacion = 0;
+    // En cada iteración, el tamaño de datos se reduce en 1, ya que eliminamos dos elementos pero añadimos uno nuevo. 
     while (datos.size() > 1){
         int suma = datos[0] + datos[1] - 1;
         comparacion += suma;
